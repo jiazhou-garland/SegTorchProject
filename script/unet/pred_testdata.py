e4c5eca23bac9f599b41d63789590171aa2a8a44
jim <rowcing@gmail.com>
2021-02-13 09:42:20 +0800

    pred testdata

---
+++ script/unet/pred_testdata.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021 - 02 - 13
+# @Author  : Luo jin
+# @User    : jim 
+# @File    : pred_testdata.py
+# -----------------------------------------
+#
